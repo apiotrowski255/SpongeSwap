93f3ac36f7b91357082d3fcec41422f12613e135
awesomea900 <awesomea900@gmail.com>
2019-06-16 13:56:03 +1000

    Idle animation for spongebob is fucked, started to work on the down animation)

--- src/entities/SpongeBob.java
+++ src/entities/SpongeBob.java
@@ -2,6 +2,8 @@ package entities;
 
 import static org.lwjgl.opengl.GL11.GL_TEXTURE_2D;
 
+import java.util.ArrayList;
+
 import org.lwjgl.opengl.GL11;
 import org.newdawn.slick.opengl.Texture;
 
@@ -12,7 +14,7 @@ public class SpongeBob extends Entity {
 	public Texture faceTexture;
 	public Texture bodyTexture;
 	
-	public float faceX, faceY;
+	public float faceX, faceY, faceYIdleDisplacement;
 	public int faceW, faceH;
 	
 	public float bodyX, bodyY;
@@ -23,6 +25,12 @@ public class SpongeBob extends Entity {
 	public float xSpeed;
 	
 	public int counter;
+	
+	private final float MAXDISPLACEMENT = 5;
+	private final float IDLEMOVEMENTSPEED = 0;
+	private boolean upMovement = true;
+	
+	public ArrayList<Texture> animationDown;
 	
 	public SpongeBob(float x, float y) {
 		super(x, y);
@@ -48,12 +56,17 @@ public class SpongeBob extends Entity {
 		
 		this.xSpeed = 4;
 		this.counter = 0;
+		
+		//Idle movement
+		this.faceYIdleDisplacement = 0;
+		
+		loadAnimationDownTextures();
 	}
 
 	public void render() {
 		GL11.glEnable(GL_TEXTURE_2D);
 		GL11.glColor3f(1, 1, 1);
-		Shapes.DrawQuadTex(faceTexture, faceX, faceY, faceW, faceH);
+		Shapes.DrawQuadTex(faceTexture, faceX, faceY + faceYIdleDisplacement, faceW, faceH);
 		Shapes.DrawQuadTex(bodyTexture, bodyX, bodyY, bodyW, bodyH);
 	}
 
@@ -79,12 +92,33 @@ public class SpongeBob extends Entity {
 		this.bodyX = super.getX();
 		this.bodyY = super.getY() + bodyYOffset;
 		
+		if (upMovement){
+			this.faceYIdleDisplacement += IDLEMOVEMENTSPEED;
+			if (faceYIdleDisplacement >= MAXDISPLACEMENT){
+				upMovement = false;
+			}
+		} else {
+			this.faceYIdleDisplacement -= IDLEMOVEMENTSPEED;
+			if (faceYIdleDisplacement < 0){
+				upMovement = true;
+			}
+		}
+		
 	}
 	
 	public void dodge(){
 		this.faceTexture = Shapes.LoadTexture("res/SpongeBob/face_wink_right.png", "PNG");
 		this.xSpeed = -4;
 		this.counter = 60;
+	}
+	
+	public void loadAnimationDownTextures(){
+		this.animationDown = new ArrayList<Texture>();
+		animationDown.add(Shapes.LoadTexture("res/SpongeBob/down1.png", "PNG"));
+		animationDown.add(Shapes.LoadTexture("res/SpongeBob/down2.png", "PNG"));
+		animationDown.add(Shapes.LoadTexture("res/SpongeBob/down3.png", "PNG"));
+		animationDown.add(Shapes.LoadTexture("res/SpongeBob/down4.png", "PNG"));
+		animationDown.add(Shapes.LoadTexture("res/SpongeBob/down5.png", "PNG"));
 	}
 
 }
